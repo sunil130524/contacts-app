54ced44ccaa8464cea7be89061db99cc8cfcb5ef
Sunil Kumar C <sunil130524@gmail.com>
2025-02-13 11:59:26 +0530

    Providing values to inputs

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
   templateUrl: './edit-contact.component.html',
@@ -12,11 +13,18 @@ export class EditContactComponent implements OnInit {
   dateOfBirth = new FormControl();
   favoritesRanking = new FormControl();
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private contactsService: ContactsService) { }
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return
+
+    this.contactsService.getContact(contactId).subscribe((contact) => {
+      this.firstName.setValue(contact?.firstName);
+      this.lastName.setValue(contact?.lastName);
+      this.dateOfBirth.setValue(contact?.dateOfBirth);
+      this.favoritesRanking.setValue(contact?.favoritesRanking);
+    })
   }
 
   saveContact() {
