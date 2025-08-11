dd3cfb93a371579fd3ba9a3172f2faecbd76bce5
Sunil Kumar C <sunil130524@gmail.com>
2025-08-12 01:12:51 +0530

    Use FormBuilder to build and initialize the form

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ContactsService } from '../contacts/contacts.service';
 
@@ -8,29 +8,30 @@ import { ContactsService } from '../contacts/contacts.service';
   styleUrls: ['./edit-contact.component.css'],
 })
 export class EditContactComponent implements OnInit {
-  contactForm = new FormGroup({
-    id: new FormControl(),
-    firstName: new FormControl(),
-    lastName: new FormControl(),
-    dateOfBirth: new FormControl(),
-    favoritesRanking: new FormControl(),
-    phone: new FormGroup({
-      phoneNumber: new FormControl(),
-      phoneType: new FormControl(),
+  contactForm = this.formBuilder.nonNullable.group({
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: <Date | null>null,
+    favoritesRanking: <number | null>null,
+    phone: this.formBuilder.nonNullable.group({
+      phoneNumber: '',
+      phoneType: '',
     }),
-    address: new FormGroup({
-      streetAddress: new FormControl(),
-      city: new FormControl(),
-      state: new FormControl(),
-      postalCode: new FormControl(),
-      addressType: new FormControl(),
+    address: this.formBuilder.nonNullable.group({
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
     }),
   });
 
   constructor(
     private route: ActivatedRoute,
     private contactsService: ContactsService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
@@ -38,36 +39,9 @@ export class EditContactComponent implements OnInit {
     if (!contactId) return;
 
     this.contactsService.getContact(contactId).subscribe((contact) => {
-      this.contactForm.controls.id.setValue(contact?.id);
-      this.contactForm.controls.firstName.setValue(contact?.firstName);
-      this.contactForm.controls.lastName.setValue(contact?.lastName);
-      this.contactForm.controls.dateOfBirth.setValue(contact?.dateOfBirth);
-      this.contactForm.controls.favoritesRanking.setValue(
-        contact?.favoritesRanking
-      );
+      if (!contact) return;
 
-      this.contactForm.controls.phone.controls.phoneNumber.setValue(
-        contact?.phone.phoneNumber
-      );
-      this.contactForm.controls.phone.controls.phoneType.setValue(
-        contact?.phone.phoneType
-      );
-
-      this.contactForm.controls.address.controls.streetAddress.setValue(
-        contact?.address.streetAddress
-      );
-      this.contactForm.controls.address.controls.city.setValue(
-        contact?.address.city
-      );
-      this.contactForm.controls.address.controls.state.setValue(
-        contact?.address.state
-      );
-      this.contactForm.controls.address.controls.postalCode.setValue(
-        contact?.address.postalCode
-      );
-      this.contactForm.controls.address.controls.addressType.setValue(
-        contact?.address.addressType
-      );
+      this.contactForm.setValue(contact);
     });
   }
 
